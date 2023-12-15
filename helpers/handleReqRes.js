07a728f700e4b2011b4cbb20ae1954ccb557be17
jakaria98 <jhossain439@gmail.com>
2023-12-15 00:40:44 -0330

    sample route and router hadler added

--- helpers/handleReqRes.js
+++ helpers/handleReqRes.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const { StringDecoder } = require('string_decoder');
-
+const routes = require('../routes');
+const { notFoundHandler } = require('../handler/routeHandlers/notFoundHandler');
 // module scaffolding
 const handler = {};
 handler.handleReqRes = (req, res) => {
@@ -11,10 +12,32 @@ handler.handleReqRes = (req, res) => {
     const path = parsedUrl.pathname;
     const trimmedPath = path.replace(/^\/+|\/+$/g, '');
     const method = req.method.toLowerCase();
+    // query string as an object
     const queryStringObject = parsedUrl.query;
     const headersObject = req.headers;
+
+    const requestProperties = {
+        parsedUrl,
+        path,
+        trimmedPath,
+        method,
+        queryStringObject,
+        headersObject,
+    };
+
     const decoder = new StringDecoder('utf-8');
     let realData = '';
+
+    const chosenHandler = routes[trimmedPath] ? routes[trimmedPath] : notFoundHandler;
+    chosenHandler(requestProperties, (statusCode, payload) => {
+        statusCode = typeof statusCode === 'number' ? statusCode : 500;
+        payload = typeof payload === 'object' ? payload : {};
+        const payloadString = JSON.stringify(payload);
+        // return the final response
+        res.writeHead(statusCode);
+        res.end(payloadString);
+    }); 
+    
     req.on('data', (buffer) => {
         realData += decoder.write(buffer);
     });
---
+++ routes.js
@@ -0,0 +1,5 @@
+const { sampleHandler } = require('./handler/routeHandlers/sampleHandler');
+const routes = {
+    sample: sampleHandler,
+};
+module.exports = routes;
