94731f67051a81bbaba77a382cf42e1f986c3280
jakaria98 <jhossain439@gmail.com>
2023-12-15 02:32:29 -0330

    environment changed to .env file

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ Author: Jakaria Hossain
 // Dependencies
 const http = require('http');
 const { handleReqRes } = require('./helpers/handleReqRes');
-const environment = require('./helpers/environments');
+// eslint-disable-next-line import/extensions
+const environment = require('./.env');
 const data = require('./lib/data');
 const lib = require('./lib/data');
 // app object - module scaffolding
@@ -15,16 +16,17 @@ const app = {};
 // testing file system
 // @TODO delete this
 data.create('test', 'newFile', { name: 'Bangladesh', language: 'Bangla' }, (err) => {
-    console.log(`Error was `, err);
+    console.log('Error was ', err);
 });
+// eslint-disable-next-line no-shadow
 data.read('test', 'newFile', (err, data) => {
-    console.log(`Error was `, err, ` and data is `, data);
+    console.log('Error was ', err, ' and data is ', data);
 });
 data.update('test', 'newFile', { name: 'America', language: 'English' }, (err) => {
-    console.log(`Error was `, err);
+    console.log('Error was ', err);
 });
 lib.delete('test', 'newFile', (err) => {
-    console.log(`Error was `, err);
+    console.log('Error was ', err);
 });
 // create server
 app.createServer = () => {
