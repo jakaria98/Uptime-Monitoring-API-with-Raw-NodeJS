ea7855c923ea5f79563529b270fbe6d2b18d8f0b
jakaria98 <jhossain439@gmail.com>
2023-12-15 23:56:29 -0330

    get route done

--- handler/routeHandlers/userHandler.js
+++ handler/routeHandlers/userHandler.js
@@ -1,7 +1,8 @@
 //dependencies
 const data = require('../../lib/data');
 const { hash } = require('../../helpers/utilities');
-// module scaffolding
+const { parseJSON } = require('../../helpers/utilities');
+ // module scaffolding
 
 const handler = {};
 handler.userHandler = (requestProperties, callback) => {
@@ -71,8 +72,31 @@ handler._users.post = (requestProperties, callback) => {
     }
 };
 handler._users.get = (requestProperties, callback) => {
-    callback(200);
-}
+    // check the phone number if valid
+    const phone =
+        typeof requestProperties.queryStringObject.phone === 'string' &&
+        requestProperties.queryStringObject.phone.trim().length === 11
+            ? requestProperties.queryStringObject.phone
+            : false;
+    if (phone) {
+        // lookup the user
+        data.read('users', phone, (err, u) => {
+            const user = { ...parseJSON(u) };
+            if (!err && user) {
+                delete user.password;
+                callback(200, user);
+            } else {
+                callback(404, {
+                    error: 'Requested user was not found!',
+                });
+            }
+        });
+    } else {
+        callback(404, {
+            error: 'Requested user was not found!',
+        });
+    }
+};
 handler._users.put = (requestProperties, callback) => {
     callback(200);
 }
