24413746c5d79e3e5290f689c6334748e87eb3df
jakaria98 <jhossain439@gmail.com>
2023-12-16 22:04:10 -0330

    token generated

--- helpers/utilities.js
+++ helpers/utilities.js
@@ -20,4 +20,20 @@ utilities.hash = (str) => {
     }
     return false;
 };
+utilities.createRandomString = (strLength) => {
+    let length = strLength;
+    length = typeof strLength === 'number' && strLength > 0 ? strLength : false;
+    if (length) {
+        const possibleCharacters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+        let output = '';
+        for (let i = 1; i <= length; i += 1) {
+            const randomCharacter = possibleCharacters.charAt(
+                Math.floor(Math.random() * possibleCharacters.length)
+            );
+            output += randomCharacter;
+        }
+        return output;
+    }
+    return false;
+};
 module.exports = utilities;
