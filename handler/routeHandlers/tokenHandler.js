0452578828ed4d0826dbe059a84dafc728f8dcae
jakaria98 <jhossain439@gmail.com>
2023-12-16 22:39:37 -0330

    token delete done

--- handler/routeHandlers/tokenHandler.js
+++ handler/routeHandlers/tokenHandler.js
@@ -121,5 +121,37 @@ handler._token.put = (requestProperties, callback) => {
         });
     }
 };
-handler._token.delete = (requestProperties, callback) => {};
+handler._token.delete = (requestProperties, callback) => {
+    const id =
+        typeof requestProperties.queryStringObject.id === 'string' &&
+        requestProperties.queryStringObject.id.trim().length === 20
+            ? requestProperties.queryStringObject.id
+            : false;
+    if (id) {
+        // lookup the user
+        data.read('tokens', id, (err1, tokenData) => {
+            if (!err1 && tokenData) {
+                data.delete('tokens', id, (err2) => {
+                    if (!err2) {
+                        callback(200, {
+                            message: 'Token was successfully deleted!',
+                        });
+                    } else {
+                        callback(500, {
+                            error: 'There was a server side error!',
+                        });
+                    }
+                });
+            } else {
+                callback(500, {
+                    error: 'There was a server side error!',
+                });
+            }
+        });
+    } else {
+        callback(400, {
+            error: 'There was a problem in your request!',
+        });
+    }
+};
 module.exports = handler;
