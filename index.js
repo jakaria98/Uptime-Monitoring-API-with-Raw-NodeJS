11d6d2adb4c9d72ac5616e3f852e2d1ea2904ada
jakaria98 <jhossain439@gmail.com>
2023-12-19 21:35:58 -0330

    index.js is partitioned in 2 parts for server and worker

--- index.js
+++ index.js
@@ -1,42 +1,25 @@
 /*
-Title: Uptime Monitoring API
-Description: an API to monitor up or down time of user defined links
+Title: Server related file
+Description: an API to monitor up or down time of user defined links / Server Related file
 Author: Jakaria Hossain
 *
 */
 // Dependencies
-const http = require('http');
-const { handleReqRes } = require('./helpers/handleReqRes');
+const server = require('./lib/server');
+const workers = require('./lib/workers');
 // eslint-disable-next-line import/extensions
 const environment = require('./.env');
 const data = require('./lib/data');
 const lib = require('./lib/data');
 // app object - module scaffolding
 const app = {};
-// testing file system
-// @TODO delete this
-// data.create('test', 'newFile', { name: 'Bangladesh', language: 'Bangla' }, (err) => {
-//     console.log('Error was ', err);
-// });
-// // eslint-disable-next-line no-shadow
-// data.read('test', 'newFile', (err, data) => {
-//     console.log('Error was ', err, ' and data is ', data);
-// });
-// data.update('test', 'newFile', { name: 'America', language: 'English' }, (err) => {
-//     console.log('Error was ', err);
-// });
-// lib.delete('test', 'newFile', (err) => {
-//     console.log('Error was ', err);
-// });
 // create server
-app.createServer = () => {
-    const server = http.createServer(app.handleReqRes);
-    server.listen(environment.port, () => {
-        console.log(`Listening to port ${environment.port}`);
-    });
+app.init = () => {
+    // start the server
+    server.createServer();
+    // start the workers
+    workers.init();
 };
 
-// handle request response
-app.handleReqRes = handleReqRes;
-// start server
-app.createServer();
+// execute
+app.init();
